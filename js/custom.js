fcac3c0e1c76380bb07847271cf81a0612aee122
devavology <145644105+devavology@users.noreply.github.com>
2024-05-21 17:49:47 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+allEventListners();
+
+
+function allEventListners() {
+
+    navToggler.addEventListener('click', togglerClick);
+
+    navLinks.forEach(elem => elem.addEventListener('click', navLinkClick));
+}
+
+
+function togglerClick() {
+    navToggler.classList.toggle('toggler-open');
+    navMenu.classList.toggle('open');
+}
+
+
+function navLinkClick() {
+    if (navMenu.classList.contains('open')) {
+        navToggler.click();
+    }
+}
+
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 200) {
+        $('header').addClass('fixed-header');
+    }
+    else {
+        $('header').removeClass('fixed-header');
+    }
+  });
